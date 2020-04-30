2a5a78fb27b3f03ebba1f507552ae67331ffaa07
uoyoCsharp <859527769@qq.com>
2020-05-01 00:22:45 +0800

    :ram: init

---
+++ src/MiCake.Autofac/MiCakeApplicationAutofacExtension.cs
@@ -0,0 +1,17 @@
+﻿using MiCake.Core;
+
+namespace MiCake.Autofac
+{
+    public static class MiCakeApplicationAutofacExtension
+    {
+        public static IMiCakeBuilder AddAutofac(this IMiCakeBuilder builder)
+        {
+            builder.ConfigureApplication(app =>
+            {
+                app.ModuleManager.AddFeatureModule(typeof(MiCakeAutofacModule));
+            });
+
+            return builder;
+        }
+    }
+}
