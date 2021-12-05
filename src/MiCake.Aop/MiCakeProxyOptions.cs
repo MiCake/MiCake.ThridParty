132df44f350b963b87c943f67783ac9fb101b4c8
uoyoCsharp <344481481@qq.com>
2021-12-05 23:14:42 +0800

    移动MiCake.Aop至本仓库

---
+++ src/MiCake.Aop/MiCakeProxyOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace MiCake.Aop
+{
+    public class MiCakeProxyOptions
+    {
+        /// <summary>
+        /// Need create a new <see cref="IMiCakeProxy"/>?
+        /// </summary>
+        public bool RequiredNew { get; set; }
+
+        public MiCakeProxyOptions()
+        {
+        }
+    }
+}
---
+++ src/MiCake.Aop/Modules/MiCakeAopModule.cs
@@ -0,0 +1,18 @@
+﻿using MiCake.Core.Modularity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MiCake.Aop.Modules
+{
+    public class MiCakeAopModule : MiCakeModule
+    {
+        public override bool IsFrameworkLevel => true;
+
+        public override void ConfigServices(ModuleConfigServiceContext context)
+        {
+            context.Services.AddTransient(provider =>
+            {
+                return provider.GetService<IMiCakeProxyProvider>().GetMiCakeProxy();
+            });
+        }
+    }
+}
